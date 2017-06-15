ddaaafdbbab7dae69993d1f2ddd5010bf1e51e26
EzraWeller <ezra.weller@gmail.com>
2017-06-15 09:21:45 -0400

    Basic setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613123343) do
+ActiveRecord::Schema.define(version: 20170614131413) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
