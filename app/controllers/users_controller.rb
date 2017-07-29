d89f66df222253d373aa3f6608aa4df23010aa31
EzraWeller <ezra.weller@gmail.com>
2017-07-29 09:47:00 -0400

    regular back up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "New user created!"
+      redirect_to show_path
+    else
+      render 'new'
+    end
+  end
+  
+  def index
+    @users = User.all
+  end
+
+private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'static_pages#home'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get  '/index',  to: 'users#index'
   resources :users
 end
