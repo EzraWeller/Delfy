59e4abdca8cb01829e465c5aa8ac1345adec5e05
EzraWeller <ezra.weller@gmail.com>
2017-08-06 16:36:51 -0400

    Working community select w js

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+    def home
+  		@user = current_user
+    end
+
+    def set_community
+    	@community = Community.find(params[:community_id])
+    	select_community(@community)
+		respond_to do |format|
+			format.html { redirect_to root_url }
+			format.js
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root                  		  'static_pages#home'
-  get    '/signup', 		  to: 'users#new'
-  post   '/signup',			  to: 'users#create'
-  get    '/index',  		  to: 'users#index'
-  get    '/login', 			  to: 'sessions#new'
-  post   '/login',  		  to: 'sessions#create'
-  delete '/logout',  		  to: 'sessions#destroy'
+  root                  		      'static_pages#home'
+  post   '/select_community', to: 'static_pages#set_community', 
+                                  as: 'select_community'
+  get    '/signup', 		      to: 'users#new'
+  post   '/signup',			      to: 'users#create'
+  get    '/index',  		      to: 'users#index'
+  get    '/login', 			      to: 'sessions#new'
+  post   '/login',  		      to: 'sessions#create'
+  delete '/logout',  		      to: 'sessions#destroy'
   post   '/communities/new',  to: 'communities#create'
   resources :users do
   	member do
