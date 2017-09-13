aec34956dd7a7ff93e5062396f42d29f9d4106d2
EzraWeller <ezra.weller@gmail.com>
2017-09-13 10:01:43 -0400

    cleanup

--- db/migrate/20170614123831_add_name_to_users.rb
+++ db/migrate/20170614123831_add_name_to_users.rb
@@ -1,4 +1,4 @@
-class AddNameAndEmailToUsers < ActiveRecord::Migration[5.0]
+class AddNameToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name,  :string
     add_column :users, :email, :string
