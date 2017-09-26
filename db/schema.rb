b8bb2e7123fa04bba9d1ce812c77bdf7f0d4e5ce
EzraWeller <ezra.weller@gmail.com>
2017-09-26 10:12:23 -0400

    halfway test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920203100) do
+ActiveRecord::Schema.define(version: 20170926044636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170920203100) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean  "admin",             default: false
   end
 
   create_table "votes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,74 +6,92 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:  "Admin Boy",
-             email: "admin@gmail.com",
-             password:              "password",
-             password_confirmation: "password",
-             activated: true,
-             activated_at: Time.zone.now)
+case Rails.env
+when "development"
+	User.create!(name:  "Admin Boy",
+	             email: "admin@gmail.com",
+	             password:              "password",
+	             password_confirmation: "password",
+	             activated: true,
+	             activated_at: Time.zone.now)
 
-30.times do |u|
-	User.create!(name:  "Mr. #{u}",
-				 email: "doe#{u}@gmail.com",
-				 password:              "password",
-				 password_confirmation: "password",
-				 activated: true,
-             	 activated_at: Time.zone.now)
-end
+	30.times do |u|
+		User.create!(name:  "Mr. #{u}",
+					 email: "doe#{u}@gmail.com",
+					 password:              "password",
+					 password_confirmation: "password",
+					 activated: true,
+	             	 activated_at: Time.zone.now)
+	end
 
-Community.create!(name: "First Community",
-				  description: "The first seed community",
-				  leader: User.first.id,
-				  membership_setting: "open")
+	Community.create!(name: "First Community",
+					  description: "The first seed community",
+					  leader: User.first.id,
+					  membership_setting: "open")
 
-Community.create!(name: "Second Community",
-				  description: "The first seed community",
-				  leader: User.last.id,
-				  membership_setting: "closed")
+	Community.create!(name: "Second Community",
+					  description: "The first seed community",
+					  leader: User.last.id,
+					  membership_setting: "closed")
 
-Community.create!(name: "Delfy",
-				  description: "Tutorial community",
-				  leader: User.first.id,
-				  membership_setting: "open")
+	Community.create!(name: "Delfy",
+					  description: "Tutorial community",
+					  leader: User.first.id,
+					  membership_setting: "open")
 
-users = User.all
-users.each { |u| u.join(Community.first)}
+	users = User.all
+	users.each { |u| u.join(Community.first)}
 
-Idea.create!(community: Community.first,
-			 user: User.first,
-			 content: "Lorem grundle fish")
-
-110.times do |b|
-	BranchIdea.create!(user: User.find_by(id: 1),
-					   idea: Idea.first,
-					   community: Community.first,
-					   content: "Lorem grundle fish the #{b+3}th")
-end
-
-10.times do |v|
-	Vote.create!(user: User.find_by(id: v+1),
-				 idea: Idea.find_by(id: 1),
-				 branch_idea: Idea.find_by(id: 1).branches.first,
-				 community: Idea.find_by(id: 1).community)
-end
-
-10.times do |v|
-	Vote.create!(user: User.find_by(id: v+11),
-				 idea: Idea.find_by(id: 1),
-				 branch_idea: Idea.find_by(id: 1).branches.second,
-				 community: Idea.find_by(id: 1).community)
-end
-
-10.times do |v|
-	Vote.create!(user: User.find_by(id: v+21),
-				 idea: Idea.find_by(id: 1),
-				 branch_idea: Idea.find_by(id: 1).branches.third,
-				 community: Idea.find_by(id: 1).community)
-end
-
-110.times do |i|
 	Idea.create!(community: Community.first,
 				 user: User.first,
-				 content: "Idea #{i+1}")
+				 content: "Lorem grundle fish")
+
+	110.times do |b|
+		BranchIdea.create!(user: User.find_by(id: 1),
+						   idea: Idea.first,
+						   community: Community.first,
+						   content: "Lorem grundle fish the #{b+3}th")
+	end
+
+	10.times do |v|
+		Vote.create!(user: User.find_by(id: v+1),
+					 idea: Idea.find_by(id: 1),
+					 branch_idea: Idea.find_by(id: 1).branches.first,
+					 community: Idea.find_by(id: 1).community)
+	end
+
+	10.times do |v|
+		Vote.create!(user: User.find_by(id: v+11),
+					 idea: Idea.find_by(id: 1),
+					 branch_idea: Idea.find_by(id: 1).branches.second,
+					 community: Idea.find_by(id: 1).community)
+	end
+
+	10.times do |v|
+		Vote.create!(user: User.find_by(id: v+21),
+					 idea: Idea.find_by(id: 1),
+					 branch_idea: Idea.find_by(id: 1).branches.third,
+					 community: Idea.find_by(id: 1).community)
+	end
+
+	110.times do |i|
+		Idea.create!(community: Community.first,
+					 user: User.first,
+					 content: "Idea #{i+1}")
+	end
+when "production"
+	User.create!(name:                  "Delfy Help",
+				 email:                 "delfyhelp@gmail.com",
+				 password:              "starterpw",
+				 password_confirmation: "starterpw",
+				 activated:             true,
+				 activated_at:          Time.zone.now,
+				 admin:                 true)
+
+	Community.create!(name:               "Delfy",
+					  description:        "This community is for gathering feedback 
+					  					  on the Delfy platform from its users. Get a sense for 
+					  					  how things work while helping us improve the platform!",
+					  leader:             User.first.id,
+					  membership_setting: "open")
 end
