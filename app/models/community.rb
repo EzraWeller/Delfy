a13656c386d0d968256ecfa2afda4b4782cad9de
EzraWeller <ezra.weller@gmail.com>
2017-09-27 07:05:43 -0400

    paginate communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -9,6 +9,7 @@ class Community < ApplicationRecord
 	validates(:name,  presence: true, length: { maximum: 100 })
 	validates(:description,  presence: true, length: { maximum: 400 })
 	validates(:leader,  presence: true)
+	paginates_per 25
 
 	def leader?(user)
 		leader == user.id
