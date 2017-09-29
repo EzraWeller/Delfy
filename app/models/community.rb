b83e3c2c4c6479393aabae083128f3ea0af3065e
EzraWeller <ezra.weller@gmail.com>
2017-09-29 09:00:42 -0400

    add errors and unique names to new community form

--- app/models/community.rb
+++ app/models/community.rb
@@ -6,7 +6,7 @@ class Community < ApplicationRecord
 	has_many :votes
 	has_many :invitations
 	default_scope -> { order(:name) }
-	validates(:name,  presence: true, length: { maximum: 100 })
+	validates(:name,  presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false })
 	validates(:description,  presence: true, length: { maximum: 400 })
 	validates(:leader,  presence: true)
 	paginates_per 25
