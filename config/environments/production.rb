80a4bb97ef31182b2db6f46da3fc9f078485601c
EzraWeller <ezra.weller@gmail.com>
2017-09-29 09:14:41 -0400

    mailer host/url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'enigmatic-oasis-78172.herokuapp.com'
+  host = 'www.delfy.us'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -70,7 +70,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'delfy.us',
     :enable_starttls_auto => true
   }
 
