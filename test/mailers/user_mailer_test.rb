40cc65d1ee6d99b52fca01234a51cfea1669ea31
EzraWeller <ezra.weller@gmail.com>
2017-11-22 21:19:15 -0500

    proper mobile design?

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+class UserMailerTest < ActionMailer::TestCase
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
